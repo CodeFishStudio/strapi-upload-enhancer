42ddd1edf6d80f74d79c4f871324299b9174de51
Talha Ismail <talha.ismail@codefishstudio.com>
2024-12-05 10:25:15 +1030

    Revert "code updated"
    This reverts commit 8e69565191cf52dda7e08d3799f4f76a804d41d7.

--- server/src/services/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const placeholder_1 = __importDefault(require("./placeholder"));
-exports.default = {
-    service: placeholder_1.default,
-};
